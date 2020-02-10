e5d6b9cafc7f128cb79a32d9b23a1ae29ccc888a
Jordan-Niisan <jordanpierre424@gmail.com>
2020-02-10 16:32:24 +0400

    Changes 5

--- Display.java
+++ Display.java
@@ -8,12 +8,12 @@ public class Display {
 		Date date = new java.util.Date();
 		System.out.println("Today's Date is: " + date);
 		
-		//Pizza Test
+		//Pizza Test - 3 Pizza
 		Pizza p = new Pizza("Large",23,"Hawain");
 		Pizza p1 = new Pizza("Small",14,"Pepperoni");
 		Pizza p2 = new Pizza("Medium",25,"Green Pepper");
 		
-		//Pizza Display
+		//Pizza Display - 3 Pizza
 		System.out.println("Hawain: " + p.getSize() + ", " + p.getCost() + ", " + p.getType());
 		System.out.println("Pepperoni: " + p1.getSize() + ", " + p1.getCost() + ", " + p1.getType());
 		System.out.println("Green Pepper: " + p2.getSize() + ", " + p2.getCost() + ", " + p2.getType());
