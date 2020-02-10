9e601e621e50cf0acc3fc223f61b9fd1bc7799b2
Jordan Pierre <danpierre424@outlook.com>
2020-02-10 15:57:00 +0400

    Changes 2

--- Display.java
+++ Display.java
@@ -6,6 +6,10 @@ public class Display {
 		// TODO Auto-generated method stub
 		Date date = new java.util.Date();
 		System.out.println("Today's Date is: " + date);
+		Pizza p = new Pizza("Large",23,"Hawain");
+		Pizza p1 = new Pizza("Small",14,"Pepperoni");
+		System.out.println("Hawain: " + p.getSize() + ", " + p.getCost() + ", " + p.getType());
+		System.out.println("Pepperoni: " + p1.getSize() + ", " + p1.getCost() + ", " + p1.getType());
 	}
 
 }
